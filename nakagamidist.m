7e45602a470dc14ea64d9c6afc3d6e88dd5f90bb
Mohammad Faisal <faisalhmohd@live.com>
2015-07-18 01:35:18 +0600

    Successfully cropped and displayed Histogram, but failed to plot Nakagami Dist Hist

---
+++ nakagamidist.m
@@ -0,0 +1,35 @@
+clear all
+close all
+clc
+
+% Reading the Image
+
+filenameimg = ['04/pre/sweep_4c/2d/4c.2dus.00410sm.img'];
+imginfo = analyze75info(filenameimg);
+img = analyze75read(imginfo);
+
+% Cropping the Image
+croppedimg = img(325:390,320:400);
+
+% B-modding it for visibility
+croppedim = abs(hilbert(croppedimg));
+cropped_bm_im = log(max(croppedim,0.05));
+
+% Displaying it
+figure(1)
+one = subplot(121);
+imagesc(cropped_bm_im)
+colormap(one, gray)
+
+% Histogramming
+two = subplot(122);
+surf(abs(cropped_bm_im))
+% bar3(abs(cropped_bm_im))
+colormap(two, summer)
+
+%Extracting row for Nakagami dist. analysis
+extractrow = 250*(croppedimg(35,:))+20
+figure(2)
+h = histfit(extractrow,[], 'nakagami');
+
+
